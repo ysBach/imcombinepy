0ea1271cbe06c0d5741d4984f346299384eb142f
ysBach <dbstn95@gmail.com>
2020-06-25 19:07:06 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup_requires = []
+install_requires = ['numpy',
+                    'astropy >= 2.0',
+                    'bottleneck']
+
+classifiers = ["Intended Audience :: Science/Research"]
+
+setup(
+    name="imcombinepy",
+    version="0.1",
+    author="Yoonsoo P. Bach",
+    author_email="dbstn95@gmail.com",
+    description="",
+    license="",
+    keywords="",
+    url="",
+    classifiers=classifiers,
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=install_requires)
